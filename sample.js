5acd463e012b2ab27ddbb6ed1710f35d2dd2f7f4
sumanth-ts <48620349+sumanth-ts@users.noreply.github.com>
2023-12-22 22:39:48 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,12 +1,15 @@
-<Box sx={{ position: 'relative', display: 'flex', alignItems: 'center' }}>
+<Box sx={{ position: 'relative', display: 'flex', alignItems: 'flex-start' }}>
       {/* Your attached div */}
       <div
         style={{
           position: 'absolute',
-          top: 0,
-          left: 0,
+          top: '50%', // Adjust this value based on your design
+          left: '4px', // Adjust this value based on your design
+          transform: 'translateY(-50%)',
           padding: '5px',
           backgroundColor: '#ffcc00', // Set your preferred color
+          border: '1px solid #ccc', // Add border for clarity
+          borderRadius: '4px', // Add border radius for rounded corners
         }}
       >
         Your Div
