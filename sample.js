bd3665fbef4db3df674cc5ef5a7655386b882055
sumanth-ts <48620349+sumanth-ts@users.noreply.github.com>
2023-12-22 22:37:01 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,19 +1,17 @@
-<StyledDiv>
-            {/* Badge with ToggleButton */}
-            <Badge color="primary" badgeContent={<ToggleButton size="small">Toggle</ToggleButton>}>
-                {/* TextField */}
-                <TextField label="Your Text" variant="outlined" />
-            </Badge>
+<Box sx={{ position: 'relative', display: 'flex', alignItems: 'center' }}>
+      {/* Your attached div */}
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          padding: '5px',
+          backgroundColor: '#ffcc00', // Set your preferred color
+        }}
+      >
+        Your Div
+      </div>
 
-            {/* Some names with background color */}
-            <div style={{ marginTop: '20px' }}>
-                <div style={{ background: 'linear-gradient(to right, #66ff66, #6699ff)', padding: '10px', display: 'inline-block', margin: '5px' }}>Name 1</div>
-                <div style={{ background: 'linear-gradient(to right, #ff99cc, #cc99ff)', padding: '10px', display: 'inline-block', margin: '5px' }}>Name 2</div>
-                <div style={{ background: 'linear-gradient(to right, #99ffcc, #ffcc99)', padding: '10px', display: 'inline-block', margin: '5px' }}>Name 3</div>
-            </div>
-
-            {/* Search button with linear gradient background */}
-            <Button variant="contained" style={{ background: 'linear-gradient(to right, #ff6666, #ffcc00)', marginTop: '20px' }}>
-                Search
-            </Button>
-        </StyledDiv>
+      {/* The TextField */}
+      <TextField label="Your Text Field" variant="outlined" />
+    </Box>
